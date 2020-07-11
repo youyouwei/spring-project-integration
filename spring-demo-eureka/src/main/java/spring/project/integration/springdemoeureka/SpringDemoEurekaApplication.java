cefbf50e00a097f368ed8577b84e3466ea29c9f8
za-jiahuiyang <huiyang.jia@zatech.com>
2020-07-11 17:36:57 +0800

    add comment

--- spring-demo-eureka/src/main/java/spring/project/integration/springdemoeureka/SpringDemoEurekaApplication.java
+++ spring-demo-eureka/src/main/java/spring/project/integration/springdemoeureka/SpringDemoEurekaApplication.java
@@ -3,6 +3,12 @@ package spring.project.integration.springdemoeureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * eureka 注册中心
+ * @description
+ * @author jia
+ * @date 2020/7/11
+ */
 @SpringBootApplication
 public class SpringDemoEurekaApplication {
 
